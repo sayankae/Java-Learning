a37167349f169e1dacfce4da01b4b27325570209
SayanKae <coolronti@gmail.com>
2024-05-05 19:53:41 +0530

    - Primitive Datatypes in Java

---
+++ Variable/src/PrimitiveDataType.java
@@ -0,0 +1,22 @@
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        boolean bool = true;
+        byte b = 23;
+        short sht = 30;
+        int i = 10000;
+        long lng = 999999999;
+        float flt = 1.4f;
+        double dbl = 3.6;
+        char c = 'c';
+
+        System.out.println("There are 8 primitive type data in Java :");
+        System.out.println("Boolean type : " + bool);
+        System.out.println("Byte type : " + b);
+        System.out.println("Short type : " + sht);
+        System.out.println("Integer type : " + i);
+        System.out.println("Long type : " + lng);
+        System.out.println("Float type : " + flt);
+        System.out.println("Double type : " + dbl);
+        System.out.println("Character type : " + c);
+    }
+}
