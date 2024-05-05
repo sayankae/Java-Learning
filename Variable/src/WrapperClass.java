3e298f9d4ede38994f6da7840c35339bf58a20fd
SayanKae <coolronti@gmail.com>
2024-05-05 21:00:25 +0530

    - Added Wrapper Class

---
+++ Variable/src/WrapperClass.java
@@ -0,0 +1,22 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        Boolean bool = true;
+        Byte b = 1;
+        Short sht = 23;
+        Integer i = 100000;
+        Long lng = 99999999999999L;
+        Float flt = 23.4f;
+        Double dbl = 3.64;
+        Character chr = 'c';
+
+        System.out.println("Common Wrapper Classes to overcome Primitive Data type problem");
+        System.out.println("Boolean type : " + bool);
+        System.out.println("Byte type : " + b);
+        System.out.println("Short type : " + sht);
+        System.out.println("Integer type : " + i);
+        System.out.println("Long type : " + lng);
+        System.out.println("Float type : " + flt);
+        System.out.println("Double type : " + dbl);
+        System.out.println("Character type : " + chr);
+    }
+}
