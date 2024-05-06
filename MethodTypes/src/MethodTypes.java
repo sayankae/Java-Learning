57fd1e249495bfaf0e4add936b683c3acb103fe6
SayanKae <coolronti@gmail.com>
2024-05-06 13:29:28 +0530

    - Added User Defined method

--- MethodTypes/src/MethodTypes.java
+++ MethodTypes/src/MethodTypes.java
@@ -1,5 +1,17 @@
-class MethodTypes{
-    public static void main(String []args){
+class MethodTypes {
 
+    public static void userDefined(String parameter) {
+        System.out.println("This is a static user defined function with parameter " + parameter);
+    }
+
+    private String userDefined(String parameter, int a) {
+        return "This is a instance user defined function with parameter " + parameter + " " + a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This main method is also a method");
+        String parameter = "(This is a parameter)";
+        userDefined(parameter);
+        System.out.println("This is an instance method " + new MethodTypes().userDefined(parameter, 0));
     }
 }
