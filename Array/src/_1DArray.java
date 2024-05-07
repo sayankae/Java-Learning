8856aee814cbc88fc6372c4b9737d754fc2b2931
SayanKae <coolronti@gmail.com>
2024-05-07 13:00:39 +0530

    - Added 1-D Array - Checked Call By Reference

---
+++ Array/src/_1DArray.java
@@ -0,0 +1,47 @@
+public class _1DArray {
+
+    private static void checkCallByRef(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Setting Element " + i + " as " + (arr[i] = i + 2));
+        }
+    }
+
+    public static void main(String[] args) {
+        //Array Initialisation
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Traversing Statically Allocated Array");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        int n = 3;
+        //Dynamic Allocation
+        arr = new int[n];
+
+        //Array element Initialization
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        System.out.println();
+        System.out.println("Traversing Dynamically Allocated Array");
+        //Traversing the Array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        int[] temp = arr.clone();
+
+        System.out.println();
+        checkCallByRef(arr);
+
+        System.out.println("Checking the Array is call by value or call by reference");
+        //Traversing the Array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Old value at index "+i+" was "+temp[i]);
+            System.out.println("New value at index "+i + " is "+arr[i]);
+        }
+
+    }
+}
