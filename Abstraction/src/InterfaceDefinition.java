4d6e9b7ff1ed9c09c0e24b1d4dfdc58a00ac298c
SayanKae <coolronti@gmail.com>
2024-05-08 19:38:53 +0530

    - Added Interface

---
+++ Abstraction/src/InterfaceDefinition.java
@@ -0,0 +1,45 @@
+interface Parent {
+    void displayParent();
+}
+
+//Class inside interface
+//Extending Interface
+interface Child extends Parent {
+    void displayChild();
+
+    class Talent {
+        String talent;
+
+        Talent(String talent) {
+            this.talent = talent;
+        }
+
+        @Override
+        public String toString() {
+            return "Talent{" +
+                    "talent='" + talent + '\'' +
+                    '}';
+        }
+    }
+}
+
+class Sunny implements Child {
+    public void displayParent() {
+        System.out.println("Parent : Painter");
+    }
+
+    public void displayChild() {
+        System.out.println("Child : Poet");
+    }
+
+}
+
+
+
+public class InterfaceDefinition {
+    public static void main(String[] args) {
+        Sunny sunny = new Sunny();
+        Sunny.Talent child = new Child.Talent("Writing Poems.");
+        System.out.println(child);
+    }
+}
